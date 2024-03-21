1e814d61c23e22866e7d842a4cca25f3b577d9d0
Michal Viwegier <beebas@wp.pl>
2024-03-21 23:51:58 +0100

    base structure from LMS

---
+++ contacts.js
@@ -0,0 +1,30 @@
+// contacts.js
+
+// - Zaimportuj moduły 'fs' i 'path' do pracy z systemem plików.
+// - Utwórz zmienną 'contactsPath' i zapisz w niej ścieżkę do pliku 'contacts.json'.
+// - Do utworzenia ścieżki wykorzystaj metody modułu 'path'.
+// - Dodaj funkcję do pracy ze zbiorem kontaktów.
+// - W funcjach wykorzystaj moduł 'fs' oraz jego metody 'readFile()' i 'writeFile()'.
+// - Zrób eksport utworzonych funkcji przez 'module.exports'.
+
+/*
+ * Skomentuj i zapisz wartość
+ * const contactsPath = ;
+ */
+
+// TODO: udokumentuj każdą funkcję
+function listContacts() {
+  // ...twój kod
+}
+
+function getContactById(contactId) {
+  // ...twój kod
+}
+
+function removeContact(contactId) {
+  // ...twój kod
+}
+
+function addContact(name, email, phone) {
+  // ...twój kod
+}
